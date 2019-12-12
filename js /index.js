df76220083ac6ed04cba185e986b15c09c65ca28
ecl5pse <fantamas20@gmail.com>
2019-12-12 18:58:08 +0200

    first task

---
+++ js /index.js
@@ -0,0 +1,28 @@
+'use strict';
+/*
+
+const user = {
+    name:"jhgfd",
+    surname:"surname"
+
+};
+
+
+const userString = JSON.stringify(user);
+localStorage.setItem("myUser", userString);
+
+const deleteButton = document.getElementById("mybutton");
+
+
+
+
+deleteButton.addEventListener("click", function () {
+    localStorage.removeItem("myUser");
+} , false);
+
+onload = function (e) {
+    console.log(JSON.parse(localStorage.getItem("myUser")));
+
+};
+
+*/
